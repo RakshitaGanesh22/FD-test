bf6306d564cef29f6d7aec59dd930dafc522fd59
Rakshita Ganesh <120761109+RakshitaGanesh22@users.noreply.github.com>
2025-01-28 10:33:17 +0530

    Update PersonalDetailsForm.jsx

--- src/Components/PersonalDetailsForm.jsx
+++ src/Components/PersonalDetailsForm.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Box, TextField, Button, Typography, Grid, Avatar, IconButton } from "@mui/material";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Grid,
+  Avatar,
+  IconButton,
+} from "@mui/material";
 import { Facebook, Twitter, Instagram } from "@mui/icons-material";
 
 const PersonalDetailsForm = () => {
   return (
     <Box
       sx={{
-        maxWidth: "800px",
+        maxWidth: "1000px",
         margin: "auto",
         p: 3,
         border: "1px solid #e0e0e0",
@@ -17,10 +25,16 @@ const PersonalDetailsForm = () => {
       {/* Header */}
       <Box
         sx={{
-          bgcolor: "#8E24AA",
+          bgcolor: "#852bd9",
           color: "white",
           p: 3,
-          borderRadius: "8px 8px 0 0",
+          borderRadius: "8px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+
+          height: { sm: "40vh", md: "25vh" },
         }}
       >
         <Typography variant="h6" fontWeight="bold">
@@ -38,73 +52,156 @@ const PersonalDetailsForm = () => {
           <Grid item xs={12} md={3} sx={{ textAlign: "center" }}>
             <Avatar
               sx={{
-                width: 80,
-                height: 80,
+                width: { xs: 80, md: 100 },
+                height: { xs: 80, md: 100 },
                 margin: "auto",
+                mt: { xs: -6, md: -9 },
                 mb: 2,
                 border: "2px solid #e0e0e0",
               }}
             />
-            <Button variant="outlined" size="small">
+            <Button
+              sx={{
+                ml: { xs: 0, md: 2 },
+                color: "#000",
+                mt: -1,
+              }}
+            >
               Upload Photo
             </Button>
-            <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-              <IconButton>
-                <Facebook sx={{ color: "#3b5998" }} />
-              </IconButton>
-              <IconButton>
-                <Twitter sx={{ color: "#1DA1F2" }} />
-              </IconButton>
-              <IconButton>
-                <Instagram sx={{ color: "#E4405F" }} />
-              </IconButton>
+            <Box sx={{ mt: 4, ml: 4 }}>
+              {[Facebook, Twitter, Instagram].map((Icon, index) => (
+                <IconButton
+                  key={index}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    width: { xs: "50vw", md: "12vw" },
+                    height: { xs: "6vh", md: "5vh" },
+                    border: "1px solid gray",
+                    borderRadius: 1,
+                    mb: 3,
+                  }}
+                >
+                  <Icon sx={{ color: "#000" }} />
+                </IconButton>
+              ))}
             </Box>
           </Grid>
 
           {/* Form Fields */}
           <Grid item xs={12} md={9}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField label="First Name" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField label="Last Name" fullWidth />
-              </Grid>
+              {[
+                { label: "First Name", id: "First" },
+                { label: "Last Name", id: "Last" },
+              ].map((field, index) => (
+                <Grid
+                  key={index}
+                  item
+                  xs={12}
+                  sm={6}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <label htmlFor={field.id} style={{ textAlign: "left" }}>
+                    {field.label}
+                  </label>
+                  <TextField id={field.id} label={field.label} fullWidth />
+                </Grid>
+              ))}
               <Grid item xs={12}>
+                <label htmlFor="Bio" style={{ textAlign: "left" }}>
+                  Bio Description
+                </label>
                 <TextField
-                  label="Bio Description"
+                  id="Bio"
+                  label="Message"
                   fullWidth
                   multiline
                   rows={3}
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField label="Email address" fullWidth helperText="Will use this email ID to send you confirmation." />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Phone number"
-                  fullWidth
-                  helperText="Will use this number if they need to contact you."
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField label="Address 1" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField label="Address 2" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField label="City" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField label="State" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField label="Country" fullWidth />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField label="Zipcode" fullWidth />
+              {[
+                {
+                  label: "Email Address",
+                  id: "email",
+                  helperText:
+                    "Will use this email ID to send you confirmation.",
+                },
+                {
+                  label: "Phone number",
+                  id: "phone",
+                  helperText:
+                    "Will use this number if they need to contact you.",
+                },
+              ].map((field, index) => (
+                <Grid
+                  key={index}
+                  item
+                  xs={12}
+                  sm={6}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <label htmlFor={field.id} style={{ textAlign: "left" }}>
+                    {field.label}
+                  </label>
+                  <TextField
+                    id={field.id}
+                    label={field.label}
+                    fullWidth
+                    helperText={field.helperText}
+                  />
+                </Grid>
+              ))}
+              {["Address 1", "Address 2"].map((label, index) => (
+                <Grid
+                  key={index}
+                  item
+                  xs={12}
+                  sm={6}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <label htmlFor={label} style={{ textAlign: "left" }}>
+                    {label}
+                  </label>
+                  <TextField id={label.toLowerCase()} label={label} fullWidth />
+                </Grid>
+              ))}
+              <Grid item xs={12}>
+                <Grid container spacing={2}>
+                  {["City", "State", "Country", "Zipcode"].map(
+                    (label, index) => (
+                      <Grid
+                        key={index}
+                        item
+                        xs={12}
+                        sm={3}
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "flex-start",
+                        }}
+                      >
+                        <label htmlFor={label} style={{ textAlign: "left" }}>
+                          {label}
+                        </label>
+                        <TextField id={label} label={label} fullWidth />
+                      </Grid>
+                    )
+                  )}
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
@@ -119,10 +216,24 @@ const PersonalDetailsForm = () => {
             mt: 3,
           }}
         >
-          <Button variant="outlined" color="primary">
+          <Button
+            variant="outlined"
+            sx={{
+              borderRadius: 5,
+              color: "black",
+              border: "1px solid black",
+            }}
+          >
             Cancel
           </Button>
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: 5,
+              backgroundColor: "#852bd9",
+              color: "white",
+            }}
+          >
             Confirm
           </Button>
         </Box>
